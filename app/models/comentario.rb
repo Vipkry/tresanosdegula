1a2a4a14b1a89e0a71bb71ad5015f9697a32916b
Gustavo Pergola <lolpsxvipkry2@gmail.com>
2017-07-17 04:21:54 +0000

    added validations

--- app/models/comentario.rb
+++ app/models/comentario.rb
@@ -1,2 +1,7 @@
 class Comentario < ApplicationRecord
+    
+    validates :desc, length: { minimum: 1, maximum: 5000 }, 
+                     allow_blank: false
+    validates :name, length: { minimum: 1, maximum: 1000 }, 
+                     allow_blank: false
 end
